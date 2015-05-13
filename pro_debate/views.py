da9733b80ceac5991ea82f78d4f537bc358b17c2
Brendan <brendan@burkesoftware.com>
2015-05-13 19:22:42 -0400

    Super basic views and templates

--- pro_debate/views.py
+++ pro_debate/views.py
@@ -4,14 +4,28 @@ from .models import Position, Elaboration
 
 
 def index(request):
-    positions = Position.objects.order_by('id')[:50]
+    # positions = Position.objects.filter().order_by('id')[:50]
+    positions = Position.objects.exclude(elaboration_of_position__tree_relation='S').exclude(elaboration_of_position__tree_relation='C').order_by('id')[:50]
+
     context = {'positions': positions}
     return render(request, 'pro_debate/index.html', context)
 
 
-def detail(request, position_id):
+def detail(request, position_id, parent_id):
     position = get_object_or_404(Position, pk=position_id)
-    children = Elaboration.objects.filter(child_of=position_id).order_by('tree_relation')
+    support_points = Elaboration.objects.filter(child_of=position_id, tree_relation='S')
+    counter_points = Elaboration.objects.filter(child_of=position_id, tree_relation='C')
 
-    context = {'position': position, 'children': children}
+    parent = None
+
+    if Position.objects.filter(pk=parent_id).first():
+    	# Need a check to make sure the parent is indeed a parent
+    	parent = Position.objects.get(pk=parent_id)
+
+    context = {
+    	'position': position, 
+    	'support_points': support_points,
+    	'counter_points': counter_points,
+    	'parent': parent
+	}
     return render(request, 'pro_debate/detail.html', context)
